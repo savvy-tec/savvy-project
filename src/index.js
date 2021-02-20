f0a61b1cc5f73888fa291e7185f797b6272f7e58
semere D <sammydug68@gmail.com>
2021-02-20 00:11:31 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDom from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  RenderContaint() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept the Request" />;
+  }
+
+  //we have to define render for class componentss
+  render() {
+    return <div className="body-rapper">{this.RenderContaint()}</div>;
+  }
+}
+ReactDom.render(<App />, document.querySelector("#root"));
